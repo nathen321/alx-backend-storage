8b1bc8890e50230396a1e68006434b9efdee7fd9
nathen321 <walidjalaly43@gmail.com>
2025-05-11 16:11:03 -0700

    cheker

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name """
+    doc = collection.update_many(
+            {'name': name}, {'$set': {"topics": topics}}
+            )
